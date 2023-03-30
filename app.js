138ba05041358004327da806da272d26418b012c
Varnahal <danielmarcelino91@gmail.com>
2023-03-30 13:24:43 -0300

    salvando categorias

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const {engine} = require('express-handlebars')
+const bodyParser = require("body-parser")
 const app = express()
 const path = require('path')
 const admin = require('./routes/admin')
-
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 //Configurações
-    app.use(express.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use(bodyParser.json())
     //handlebars
     app.engine('handlebars',engine({defaultLayout:'main'}))
     app.set('view engine', 'handlebars')
     //mongoose
-        //em breve
+        mongoose.connect('mongodb+srv://varnahal:chuvachu@varnahal.wu5lufq.mongodb.net/?retryWrites=true&w=majority')
+        .then(()=>{
+            console.log('conectado a base de dados')
+            
+        })
+        .catch((err)=>{console.log(err)})
     //Public
         app.use(express.static(path.join(__dirname,"public")))
 //Rotas
